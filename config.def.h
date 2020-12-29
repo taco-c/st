be12f9876f8c13016094cb4ed380c51694efd2bb
Rikard Karlsen <rk@cassettian.space>
2020-12-29 20:31:29 +0100

    Color tweaking

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -112,7 +112,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"black",
+	"gray20",
 };
 
 
