abb0230a14e993e8e173a127fcea6b6f5b804faf
Rikard Karlsen <rk@cassettian.space>
2020-12-29 22:20:56 +0100

    Patch alpha focus highlight

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.9, alphaUnfocused = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -121,9 +121,10 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
+unsigned int bg = 258, bgUnfocused = 258;
 
 /*
  * Default shape of cursor
